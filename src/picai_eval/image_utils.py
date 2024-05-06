f35b4702016a94c54de3943e8d23b85cedf98a2e
NataliaAlves13 <50765571+NataliaAlves13@users.noreply.github.com>
2024-05-06 12:44:06 +0200

    Change `image_utils.py` to work with nnunetv2  (#17)
    * Change `image_utils.py` to work with nnunetv2 predictions and ground truth labels with multi-class segmentations. (#17)
    * Update supported Python versions to 3.8 - 3.11
    * Increase version to 1.4.6
    
    ---------
    
    Co-authored-by: Joeran Bosma <joeran@bosma.co>

--- src/picai_eval/image_utils.py
+++ src/picai_eval/image_utils.py
@@ -69,7 +69,10 @@ def read_image(path: PathLike):
     elif '.nii' in path or '.mha' in path or 'mhd' in path:
         return sitk.GetArrayFromImage(sitk.ReadImage(path))
     elif '.npz' in path:
-        return np.load(path)['softmax'].astype('float32')[1]  # nnUnet format
+        # read the nnU-Net format
+        data = np.load(path)
+        data = data["softmax"] if "softmax" in data else data["probabilities"]
+        return data.astype("float32")[1]
     else:
         raise ValueError(f"Unexpected file path. Supported file formats: .nii(.gz), .mha, .npy and .npz. Got: {path}.")
 
